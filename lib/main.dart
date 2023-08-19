922faf18b3d105a3b00402bb4fbecc4db0740d31
MhmdIbrahim1 <107378571+MhmdIbrahim1@users.noreply.github.com>
2023-08-19 06:59:13 +0300

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -31,12 +31,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Laza Web Adder',
       theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
+        primarySwatch: Colors.grey,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Laza Web Adder'),
     );
   }
 }
